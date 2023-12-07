c177e6e4555ec506d28feb95bb0321e10ad13d90
Creepyfie <fynjybjyb@mail.ru>
2023-12-07 18:43:40 +0300

    создали PersonMapper

--- src/main/java/ru/krutov/crudspring/dao/PersonDao.java
+++ src/main/java/ru/krutov/crudspring/dao/PersonDao.java
@@ -1,5 +1,7 @@
 package ru.krutov.crudspring.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.krutov.crudspring.models.Person;
 
@@ -9,64 +11,19 @@ import java.util.List;
 
 @Component
 public class PersonDao {
-    private static  int Peopple_count;
-    private  static final String URL = "jdbc:postgresql://localhost:5432/first_db";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "140502";
 
-    private  static Connection connection;
-
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-        } catch (SQLException throwables) {
-            throw new RuntimeException(throwables);
-        }
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public PersonDao(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate=jdbcTemplate;
     }
 
     public List<Person> index(){
-        List<Person> people = new ArrayList<>();
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "SELECT * from Person";
-            ResultSet resultSet = statement.executeQuery(SQL);
-            while (resultSet.next()){
-                Person person = new Person();
-                person.setId(resultSet.getInt("id"));
-                person.setName(resultSet.getString("name"));
-                person.setAge(resultSet.getInt("age"));
-                person.setEmail(resultSet.getString("email"));
-
-                people.add(person);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return people;
+        return jdbcTemplate.query("Select * From Person", new PersonMapper());
     }
    public  Person show(int id){
-        Person person = null;
-       try {
-           PreparedStatement preparedStatement =
-                   connection.prepareStatement("SELECT * FROM  Person Where id = ?");
-           preparedStatement.setInt(1, id);
-           ResultSet resultSet =  preparedStatement.executeQuery();
-           resultSet.next();
-           person = new Person();
-           person.setId(resultSet.getInt("id"));
-           person.setName(resultSet.getString("name"));
-           person.setAge(resultSet.getInt("age"));
-           person.setEmail(resultSet.getString("email"));
-
-       } catch (SQLException e) {
-           throw new RuntimeException(e);
-       }
-       return person;
+        return jdbcTemplate.query("Select * From Person Where id = ?", new Object[]{id}, new PersonMapper())
+                .stream().findAny().orElse(null);
     }
     public void save(Person person){
 
---
+++ src/main/java/ru/krutov/crudspring/dao/PersonMapper.java
@@ -0,0 +1,19 @@
+package ru.krutov.crudspring.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.krutov.crudspring.models.Person;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+    @Override
+    public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Person person = new Person();
+        person.setId(rs.getInt("id"));
+        person.setName(rs.getString("name"));
+        person.setAge(rs.getInt("age"));
+        person.setEmail(rs.getString("email"));
+        return person;
+    }
+}
